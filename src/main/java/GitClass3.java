4aaad414fbd89e97c1783ab5ca09795272a8da32
Nataliya <nataliyakl8@icloud.com>
2023-06-23 19:59:21 -0500

    branch3

--- src/main/java/GitClass3.java
+++ src/main/java/GitClass3.java
@@ -3,5 +3,7 @@ public class GitClass3 {
     public static void main(String[] args) {
         System.out.println("new branch ");
         System.out.println("master changes");
+
+        //this change is branch # 3
     }
 }
